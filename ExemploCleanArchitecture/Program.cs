ea5a02c1ea3c872a2f11d1df7403141cfed3de56
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-12-14 21:20:29 -0300

    adicionado padrão de endpoints em lower case

--- ExemploCleanArchitecture/Program.cs
+++ ExemploCleanArchitecture/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddInfraestructure(builder.Configuration);
 builder.Services.AddInfrastructureJWT(builder.Configuration);
 builder.Services.AddInfraestructureSwagger();
 
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
